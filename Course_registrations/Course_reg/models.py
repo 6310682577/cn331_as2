8347ee9dc9949dc046423a187e85c9f20489a3ad
Bulakorn Junboonmee <69451712+6310682577@users.noreply.github.com>
2022-09-06 14:25:42 +0700

    modify model.py add subject.xlsx

--- Course_registrations/Course_reg/models.py
+++ Course_registrations/Course_reg/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    subject = models.CharField(max_length=50)
+    subject_id = models.CharField(max_length=50)
+    credit = models.IntegerField()
+    seat = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.id}: {self.subject_id} {self.subject} {self.credit} {self.seat}'
+    
+class Date(models.Model):
+    section = models.ForeignKey(Course, on_delete=models.CASCADE)
+    time = models.CharField(max_length=50)
+    day = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.id}: {self.time} {self.day}'
+    
