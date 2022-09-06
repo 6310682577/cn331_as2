1552716714ae29a3294a236212456a2dc3893bb9
Bulakorn Junboonmee <69451712+6310682577@users.noreply.github.com>
2022-09-06 13:15:37 +0700

    create urls.py modify views.py Create superuser

--- Course_registrations/Course_registrations/settings.py
+++ Course_registrations/Course_registrations/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Course_reg\',
+    'Course_reg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
