8709cc1e539cc6a3ab1d003b5a27cd1e2e39d0eb
6310682684 <69451680+6310682684@users.noreply.github.com>
2022-09-17 15:37:38 +0700

    del corse_reg

--- Course_registrations/Course_reg/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Course, Date, Student
-
-# Register your models here.
-
-admin.site.register(Course)
-admin.site.register(Date)
-admin.site.register(Student)
