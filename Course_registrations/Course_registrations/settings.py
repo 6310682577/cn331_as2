044611f435302613d39b270e1c2f2b7b78ccf921
Bulakorn Junboonmee <69451712+6310682577@users.noreply.github.com>
2022-09-23 14:29:00 +0700

    mov

--- Course_registrations/Course_registrations/settings.py
+++ Course_registrations/Course_registrations/settings.py
@@ -124,5 +124,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT='staticfiles'
+
 import django_heroku
 django_heroku.settings(locals())
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Course_registrations/Course_registrations.wsgi
