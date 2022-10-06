88a31eb471176ef0f7d974b94812846c8e6cfc25
Bulakorn Junboonmee <69451712+6310682577@users.noreply.github.com>
2022-10-06 10:00:42 +0700

    add create user

--- Course_registrations/Course_reg/models.py
+++ Course_registrations/Course_reg/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+@receiver(post_save, sender=User)
+def create_user_picks(sender, instance, created, **kwargs):
+    if created:
+        Student.objects.create(name=instance)
 
 class Course(models.Model):
     subject = models.CharField(max_length=50)
